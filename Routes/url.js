82f4fcee93fac1ceaf2667563cb32e08807971cb
venkatsp431 <sealvenki@gmail.com>
2023-09-22 12:16:09 +0530

    backend1

--- Routes/url.js
+++ Routes/url.js
@@ -28,23 +28,21 @@ router.post("/shorten", async (req, res) => {
   }
 });
 
-// Redirect to the original long URL when a short URL is accessed
 router.get("/:shortUrl", async (req, res) => {
   try {
     const { shortUrl } = req.params;
 
-    // Find the URL record by short URL
+
     const url = await Url.findOne({ shortUrl });
 
     if (!url) {
       return res.status(404).json({ message: "URL not found" });
     }
 
-    // Increment click count
     url.clickCount += 1;
     await url.save();
 
-    // Redirect to the original long URL
+
     res.redirect(url.longUrl);
   } catch (error) {
     console.error(error);
@@ -52,24 +50,23 @@ router.get("/:shortUrl", async (req, res) => {
   }
 });
 
-// Retrieve statistics
+
 router.get("/statistics", async (req, res) => {
   try {
     const today = new Date();
     const lastMonth = new Date(today);
     lastMonth.setMonth(lastMonth.getMonth() - 1);
 
-    // Total URLs created per day
+ 
     const urlsPerDay = await Url.countDocuments({
       createdAt: { $gte: today },
     });
 
-    // Total URLs created per month
     const urlsPerMonth = await Url.countDocuments({
       createdAt: { $gte: lastMonth },
     });
 
-    // Click count for each short URL
+
     const clickCounts = await Url.find({}, "shortUrl clickCount");
 
     res.status(200).json({
